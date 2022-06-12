2cf0ea9174fe2e6d569c1eeaabb341e5b481c38e
EL KHAYDER Zakaria <zelkhayder@gmail.com>
2022-06-12 23:15:51 +0100

    My solution to the clock exercise, page 55

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,33 @@ mod models;
 
 use std::{f64::consts::PI, time::Instant};
 
-use crate::models::{axis::Axis, matrices::Matrices, tuples::Tuple};
+use crate::models::{axis::Axis, canvas::Canvas, colors::Color, matrices::Matrices, tuples::Tuple};
 
 fn main() {
     let started_at = Instant::now();
 
-    let point = Tuple::point(2.0, 3.0, 4.0);
+    let size = 50;
 
-    println!(
-        "{}",
-        point.apply(&[
-            &Matrices::rotation(Axis::X, PI),
-            &Matrices::scaling(2., 3., -2.),
-            &Matrices::translation(10., -2., 5.54)
-        ])
-    );
+    let mut canvas = Canvas::new(100, 100, None);
+
+    let point = Tuple::point(f64::from(size / 2), f64::from(size / 4), 0.0);
+
+    let mut transformed_point: Tuple;
+
+    let (mut x, mut y): (u32, u32);
+
+    for i in 0..12 {
+        transformed_point = &Matrices::rotation(Axis::Z, i as f64 * PI / 6.) * &point;
+
+        x = (size as f64 + transformed_point.x) as u32;
+        y = (size as f64 + transformed_point.y) as u32;
+
+        println!("x = {}, y = {}", x, y);
+
+        canvas.write(x, y, &Color::new(1., 1., 1.));
+    }
+
+    canvas.save();
 
     println!("Program took: {:?}", started_at.elapsed());
 }
